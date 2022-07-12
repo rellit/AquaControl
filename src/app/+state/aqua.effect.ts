f4935c893d542d50c1bf89d187a5a0426c9890d1
Tim <mail@timhaller.de>
2022-07-12 20:47:07 +0200

    Callstack

--- src/app/+state/aqua.effect.ts
+++ src/app/+state/aqua.effect.ts
@@ -4,29 +4,40 @@ import { of } from 'rxjs';
 
 import { catchError, map, switchMap } from 'rxjs/operators';
 import { AquaService } from '../services/aqua.service';
-import { loadedRelayState, loadedSettings, loadRelayState, loadRelayStateError, loadSettings, loadSettingsError } from './aqua.actions';
+import {
+  loadedRelayState,
+  loadedSettings,
+  loadRelayState,
+  loadRelayStateError,
+  loadSettings,
+  loadSettingsError,
+} from './aqua.actions';
 
 @Injectable()
 export class AquaEffects {
+  loadSettings$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(loadSettings),
+      switchMap((a) =>
+        this.aquaService.getSettings().pipe(
+          map((settings) => loadedSettings(settings)),
+          catchError((error) => of(loadSettingsError({ error })))
+        )
+      )
+    )
+  );
 
-  loadSettings$ = createEffect(() => this.actions$.pipe(
-    ofType(loadSettings),
-    switchMap(a => this.aquaService.getSettings().pipe(
-      map(settings => loadedSettings(settings)),
-      catchError(error => of(loadSettingsError({ error })))
-    )),
-  ));
+  loadRelayState$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(loadRelayState),
+      switchMap((a) =>
+        this.aquaService.getRelayState().pipe(
+          map((relayState) => loadedRelayState(relayState)),
+          catchError((error) => of(loadRelayStateError({ error })))
+        )
+      )
+    )
+  );
 
-  loadRelayState$ = createEffect(() => this.actions$.pipe(
-    ofType(loadRelayState),
-    switchMap(a => this.aquaService.getRelayState().pipe(
-      map(relayState => loadedRelayState(relayState)),
-      catchError(error => of(loadRelayStateError({ error })))
-    )),
-  ));
-
-  constructor(
-    private aquaService: AquaService,
-    private actions$: Actions) { }
-
+  constructor(private aquaService: AquaService, private actions$: Actions) {}
 }
